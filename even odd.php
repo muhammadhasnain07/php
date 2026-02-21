16690d0ac1de6667617c99f2802a196eba650906
muhammadhasnain07 <hmhsabbasiaptech07@gmail.com>
2026-02-22 03:43:31 +0500

    22/2/2/2026

---
+++ even odd.php
@@ -0,0 +1,31 @@
+<?php
+//even num
+// for($a = 1; $a <= 10; $a++){
+
+//     if($a % 2 != 0){
+//         continue; 
+//     }
+
+//     echo $a . " is Even <br>";
+// }
+//odd num
+// for($a = 1; $a <= 10; $a++){
+
+//     if($a % 2 == 0){
+//         continue; 
+//     }
+
+//     echo $a . " is Odd <br>";
+// }
+
+for($a = 1; $a <= 10; $a++){
+
+    if($a % 2 == 0){
+        echo $a . " is Even <br>";
+    } else {
+        echo $a . " is Odd <br>";
+    }
+
+}
+
+?>
---
+++ loop.php
@@ -0,0 +1,35 @@
+<?php
+// // for loop
+// for($a = 1; $a <= 10; $a++){
+//     echo $a . '<br>';
+// }
+
+// for($a = 10; $a >= 1; $a--){
+//     echo $a . '<br>';
+// }
+
+// while loop
+// $a = 1;
+
+// while($a <= 10){
+//     echo $a . ' this is while loop <br>';
+//     $a++;
+// }
+
+// do while loop
+// $a = 1;
+
+// do{
+//     echo $a . ' this is do while loop <br>';
+//     $a++;
+// }while($a <= 0);
+
+
+$a = 1;
+
+do {
+    echo "2 x " . $a . " = " . (2 * $a) . "<br>";
+    $a++;
+} while ($a <= 10);
+
+?>
