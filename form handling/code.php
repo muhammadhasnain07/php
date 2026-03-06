9a4955a739e3d07e3703caee52ff3014e6f970a5
muhammadhasnain07 <hmhsabbasiaptech07@gmail.com>
2026-03-06 22:23:26 +0500

    06/03/2026 new

--- form handling/code.php
+++ form handling/code.php
@@ -1,25 +1,82 @@
 <?php
+
 include 'connection.php';
 
-if(isset($_POST['register'])){
+// INSERT
 
+if(isset($_POST['register'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $pass = $_POST['pass'];
     $address = $_POST['address'];
+    $role = $_POST['role'];
 
-    // echo $name . '<br>' . $email . '<br>' . $pass . '<br>' . $address;
+    // echo $name . '<br>'  .$email . '<br>'  .$pass . '<br>'  .$address;  
 
-    $insert = mysqli_query($con, "INSERT into register(Name, Email, Password, Address) VALUES
-    ('$name', '$email', '$pass', '$address')");
 
-    if($insert){
+    $insert  = mysqli_query($con , "INSERT INTO register(Name , Email , Password , Address , role)VALUES
+    ('$name' , '$email' , '$pass' , '$address' , '$role')");
+
+       if($insert){
+       header('location: register.php');
+       }
+    
+
+}
+
+// Update
+
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $address = $_POST['address'];
+    $role = $_POST['role'];
+
+    $update = mysqli_query($con , "UPDATE register 
+    SET
+    Name = '$name',
+    Email = '$email',
+    Password = '$password',
+    Address = '$address',
+    role = '$role'
+    where id = '$id'
+    ");
+
+    if($update){
         echo "<script>
-        alert('Data inserted successfully')
-        location.assign('register.php')
-        </script>";
+            alert('Data Updated Successfully');
+            location.assign('fetch.php');
+            </script>";
+    }
 
-        // header("location: register.php");
+}
+
+
+
+
+
+
+
+
+
+
+// DELETE
+
+if(isset($_POST['delete'])){
+    $id = $_POST['id'];
+
+    $delete = mysqli_query($con , "DELETE from register where id = '$id'");
+
+    if($delete){
+
+    echo "<script>
+        alert('Data Deleted Successfully')
+        location.assign('fetch.php')
+    </script>";
+
     }
 }
+
 ?>
